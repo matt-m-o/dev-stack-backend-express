2a4925834f0cbba67b1a0626892b0ffbf0a6d1e5
MatheusM8 <matheusmoura8@hotmail.com>
2022-08-12 00:24:05 -0300

    renamed entity 'props' to 'attributes'

--- src/api/core/Entity.ts
+++ src/api/core/Entity.ts
@@ -2,10 +2,10 @@ import { v4 } from 'uuid'
 
 export abstract class Entity<T> {
     readonly id: string;
-    public props: T;    
+    public attributes: T;    
 
-    constructor (props: T, id?: string) {
-        this.props = props;
+    constructor (attributes: T, id?: string) {
+        this.attributes = attributes;
         this.id = id ?? v4();        
     }
 }
